81285487f13a3ae566e44ca8c4a57d0fbeabfbc0
ezequiel1985 <ezequieljaureguiberry@hotmail.com>
2022-03-30 07:58:28 -0300

    se borra info que viene en app.css

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -14,10 +14,10 @@ const NavBar = () => {
                     <a href="/" target="_blank">Ofertas</a>
                     <a href="https://api.whatsapp.com/send?phone=542615029188" target="_blank">Whatsapp</a>
                     <CartWidget />
-                    {/*<a href="/" class="icon" onclick="myFunction()" target="_blank">
+                    <a href="/" class="icon" onclick="myFunction()" target="_blank">
                     <img src='./images/cartIcon.svg'/>
                      4
-                    </a>*/}
+                    </a>
                 </div>
             </nav>
             )
